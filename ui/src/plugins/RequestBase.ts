e2b82bbce29b77f32e77d30cd00354d16dc251a6
w.c <52445078@qq.com>
2023-09-04 16:52:03 +0800

    fix: adjust axios request timeout to 10 minutes, since reading files from gitlab.com is pretty much slow

--- ui/src/plugins/RequestBase.ts
+++ ui/src/plugins/RequestBase.ts
@@ -2,7 +2,7 @@ import axios, { AxiosInstance } from "axios";
 import { ElMessage } from "element-plus";
 import router from "./../router";
 export class RequestBase {
-  private readonly TIMEOUT = 100000;
+  private readonly TIMEOUT = 60 * 10 * 1000;
 
   protected request: AxiosInstance;
 
