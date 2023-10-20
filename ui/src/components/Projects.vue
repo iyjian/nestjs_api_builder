a1175cfb01ca59514d39b3c6d5da80a33f25828a
Gloria_X <656369946@qq.com>
2023-10-20 09:54:53 +0800

    format project.vue

--- ui/src/components/Projects.vue
+++ ui/src/components/Projects.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="toolbar">
-    <el-button type="primary" :icon="Plus" @click="openSubmitForm">新建项目</el-button>
+    <el-button type="primary" :icon="Plus" @click="openSubmitForm"
+      >新建项目</el-button
+    >
   </div>
 
   <div class="tableView">
@@ -12,16 +14,28 @@
       <el-table-column label="操作" width="180">
         <template #default="scope">
           <!-- <el-button type="danger" :icon="Delete" size="small"/> -->
-          <el-button :icon="Edit" size="small" @click="openEditForm(scope.row.id)" />
+          <el-button
+            :icon="Edit"
+            size="small"
+            @click="openEditForm(scope.row.id)"
+          />
         </template>
       </el-table-column>
     </el-table>
   </div>
 
-  <el-dialog v-model="dialog.visible" :show-close="false" :title="dialogTitle" @close="resetPostData">
+  <el-dialog
+    v-model="dialog.visible"
+    :show-close="false"
+    :title="dialogTitle"
+    @close="resetPostData"
+  >
     <el-form :model="postData" label-width="120px">
       <el-form-item label="仓库项目名称">
-        <el-input v-model="postData.repoName" placeholder="仅支持英文、数字、_、-" />
+        <el-input
+          v-model="postData.repoName"
+          placeholder="仅支持英文、数字、_、-"
+        />
       </el-form-item>
       <el-form-item label="项目名称">
         <el-input v-model="postData.name" />
@@ -29,8 +43,16 @@
     </el-form>
     <template #footer>
       <span class="dialog-footer">
-        <el-button @click="dialog.visible = false" :loading="dialog.button.loading">取消</el-button>
-        <el-button type="primary" @click="submit" :loading="dialog.button.loading">
+        <el-button
+          @click="dialog.visible = false"
+          :loading="dialog.button.loading"
+          >取消</el-button
+        >
+        <el-button
+          type="primary"
+          @click="submit"
+          :loading="dialog.button.loading"
+        >
           确认
         </el-button>
       </span>
@@ -40,59 +62,58 @@
 
 <script lang="ts">
 export default {
-  name: 'Projects',
-}
+  name: "Projects",
+};
 </script>
 
 <script lang="ts" setup>
-import { ref, watch, computed, shallowRef, reactive } from 'vue'
-import { devToolApiClient } from '@/plugins'
-import _ from 'lodash'
-import { Plus, Delete, Edit } from '@element-plus/icons-vue'
-import { Project } from '@/types'
+import { ref, watch, computed, shallowRef, reactive } from "vue";
+import { devToolApiClient } from "@/plugins";
+import _ from "lodash";
+import { Plus, Delete, Edit } from "@element-plus/icons-vue";
+import { Project } from "@/types";
 
-let table = reactive<Project[]>([])
+let table = reactive<Project[]>([]);
 
 async function refreshTable() {
-  table = await devToolApiClient.getAllProjects()
+  table = await devToolApiClient.getAllProjects();
 }
 
 const dialog = reactive({
   visible: false,
-  type: 'add',
+  type: "add",
   button: {
     loading: false,
   },
-})
+});
 
 const dialogTitle = computed(
-  () => `${dialog.type === 'add' ? '新建' : '修改'}项目`,
-)
+  () => `${dialog.type === "add" ? "新建" : "修改"}项目`
+);
 
 const DEFAULT_POST_DATA = {
   repoId: 0,
-  repo: '',
-  name: '',
-  repoName: '',
-  projectName: '',
-  version: '2',
-}
+  repo: "",
+  name: "",
+  repoName: "",
+  projectName: "",
+  version: "2",
+};
 
-const postData = ref(_.cloneDeep(DEFAULT_POST_DATA))
-
+const postData = ref(_.cloneDeep(DEFAULT_POST_DATA));
 
 function resetPostData() {
-  postData.value = _.cloneDeep(DEFAULT_POST_DATA)
+  postData.value = _.cloneDeep(DEFAULT_POST_DATA);
 }
 
 async function openSubmitForm() {
-  dialog.visible = true
-  dialog.type = 'add'
+  dialog.visible = true;
+  dialog.type = "add";
 }
 
 async function submit() {
   try {
-    dialog.button.loading = true
+    dialog.button.loading = true;
 
     // 从模板项目初始化
     // const result = await devToolApiClient.initProject({
@@ -103,17 +124,17 @@ async function submit() {
     // postData.value.repo = result.ssh_url_to_repo
     // postData.value.repoId = result.id
 
-    if (dialog.type === 'add') {
-      await devToolApiClient.postProject(postData.value)
+    if (dialog.type === "add") {
+      await devToolApiClient.postProject(postData.value);
     } else {
-      await devToolApiClient.updateProject(postData.value)
+      await devToolApiClient.updateProject(postData.value);
     }
-    await refreshTable()
-    dialog.visible = false
+    await refreshTable();
+    dialog.visible = false;
   } catch (e) {
-    console.log(e)
+    console.log(e);
   } finally {
-    dialog.button.loading = false
+    dialog.button.loading = false;
   }
 }
 
@@ -122,12 +143,12 @@ async function edit(projectId: number) {
 }
 
 async function openEditForm(projectId: number) {
-  postData.value = await devToolApiClient.getProjectInfo(projectId)
-  dialog.type = 'edit'
-  dialog.visible = true
+  postData.value = await devToolApiClient.getProjectInfo(projectId);
+  dialog.type = "edit";
+  dialog.visible = true;
 }
 
-await refreshTable()
+await refreshTable();
 </script>
 
 <style lang="stylus" scoped>
