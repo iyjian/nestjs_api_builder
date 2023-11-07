cf72708e7dd4a901714676559d14cbc80711679c
wc <wc@cruisesh.com>
2023-11-07 15:30:11 +0800

    upgrade openai api

--- api/src/features/third/openai.service.ts
+++ api/src/features/third/openai.service.ts
@@ -1,28 +1,33 @@
 import { ConfigService } from '@nestjs/config'
 import { CACHE_MANAGER, Inject, Injectable, Logger } from '@nestjs/common'
-import {
-  Configuration,
-  CreateCompletionResponse,
-  CreateImageRequestSizeEnum,
-  OpenAIApi,
-} from 'openai'
+// import {
+//   Configuration,
+//   CreateCompletionResponse,
+//   CreateImageRequestSizeEnum,
+//   OpenAIApi,
+// } from 'openai'
+import OpenAI from 'openai'
 import tunnel from 'tunnel'
 import { Spark } from 'iflytek-spark-nodejs'
+import fs from 'fs'
+import { Readable } from 'stream'
 
 @Injectable()
 export class OpenAPIService {
-  private readonly configuration: Configuration
+  // private readonly configuration: Configuration
 
   private readonly agent: any
 
-  private readonly openai: OpenAIApi
+  private readonly openai: OpenAI
   private readonly logger = new Logger(OpenAPIService.name)
 
   constructor(private readonly configService: ConfigService) {
-    this.configuration = new Configuration({
+    // this.configuration = new Configuration({
+    //   apiKey: this.configService.get('openAI.apiKey'),
+    // })
+    this.openai = new OpenAI({
       apiKey: this.configService.get('openAI.apiKey'),
     })
-    this.openai = new OpenAIApi(this.configuration)
 
     this.agent = tunnel.httpsOverHttp({
       proxy: {
@@ -33,34 +38,32 @@ export class OpenAPIService {
   }
 
   async listModels(): Promise<any> {
-    const result = await this.openai.listModels({
-      httpsAgent: this.agent,
+    this.openai.models.list
+    const result = await this.openai.models.list({
+      httpAgent: this.agent,
     })
     return result.data
   }
 
-  async codeCompletion() {
-    this.openai.createCompletion(
-      {
-        model: 'code-davinci-002',
-        prompt: '',
-        temperature: 0,
-        max_tokens: 150,
-        top_p: 1.0,
-        frequency_penalty: 0.0,
-        presence_penalty: 0.0,
-        stop: ['#', ';'],
-      },
-      { httpsAgent: this.agent },
-    )
-  }
+  // async codeCompletion() {
+  //   this.openai.chat.completions.create(
+  //     {
+  //       model: 'code-davinci-002',
+  //       prompt: '',
+  //       temperature: 0,
+  //       max_tokens: 150,
+  //       top_p: 1.0,
+  //       frequency_penalty: 0.0,
+  //       presence_penalty: 0.0,
+  //       stop: ['#', ';'],
+  //     },
+  //     { httpsAgent: this.agent },
+  //   )
+  // }
 
-  async textCompletion(
-    model: 'text-davinci-003',
-    prompt: string,
-  ): Promise<CreateCompletionResponse> {
+  async textCompletion(model: 'text-davinci-003', prompt: string) {
     this.logger.debug(`textCompletion - prompt: ${prompt}`)
-    const result = await this.openai.createCompletion(
+    const result = await this.openai.completions.create(
       {
         model,
         prompt,
@@ -71,9 +74,28 @@ export class OpenAPIService {
         presence_penalty: 1.0,
         stop: ['#', ';'],
       },
-      { httpsAgent: this.agent },
+      { httpAgent: this.agent },
     )
-    return result.data
+    return result
+  }
+
+  async tts(
+    model: 'tts-1' | 'tts-1-hd',
+    input: string,
+    voice: 'alloy' | 'echo' | 'fable' | 'onyx' | 'nova' | 'shimmer',
+  ) {
+    const result = await this.openai.audio.speech.create(
+      {
+        model,
+        input,
+        voice,
+      },
+      {
+        httpAgent: this.agent,
+      },
+    )
+    const content = await result.arrayBuffer()
+    return Readable.from(Buffer.from(content))
   }
 
   async chatCompletion(
@@ -82,7 +104,7 @@ export class OpenAPIService {
   ) {
     try {
       this.logger.debug(`chatCompletion - begin - model: ${model}`)
-      const result = await this.openai.createChatCompletion(
+      const result = await this.openai.chat.completions.create(
         {
           model,
           // messages: [{ role: 'user', content: '你好呀' }],
@@ -94,9 +116,9 @@ export class OpenAPIService {
           presence_penalty: 0.0,
           stop: ['#', ';'],
         },
-        { httpsAgent: this.agent },
+        { httpAgent: this.agent },
       )
-      return result.data
+      return result
     } catch (e) {
       console.log(e.message)
       console.log(e.response.data)
@@ -105,34 +127,34 @@ export class OpenAPIService {
   }
 
   async imageGen(prompt: string, size: string) {
-    const result = await this.openai.createImage(
+    const result = await this.openai.images.generate(
       {
         size:
           size === 'small'
-            ? CreateImageRequestSizeEnum._256x256
+            ? '256x256'
             : size === 'medium'
-            ? CreateImageRequestSizeEnum._512x512
-            : CreateImageRequestSizeEnum._1024x1024,
+            ? '512x512'
+            : '1024x1024',
         n: 1,
         prompt,
         response_format: 'b64_json',
       },
-      { httpsAgent: this.agent },
+      { httpAgent: this.agent },
     )
     return result.data
   }
 
-  async getEmbedding(inputs: string[], user?: string) {
-    const response = await this.openai.createEmbedding(
-      {
-        model: 'text-embedding-ada-002',
-        input: inputs,
-        user,
-      },
-      { httpsAgent: this.agent },
-    )
-    return response.data
-  }
+  // async getEmbedding(inputs: string[], user?: string) {
+  //   const response = await this.openai.createEmbedding(
+  //     {
+  //       model: 'text-embedding-ada-002',
+  //       input: inputs,
+  //       user,
+  //     },
+  //     { httpsAgent: this.agent },
+  //   )
+  //   return response.data
+  // }
 
   async chatCompletionSpark(model: 'v1' | 'v2' = 'v2', content: string) {
     const spark = new Spark({
