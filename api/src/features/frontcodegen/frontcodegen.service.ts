96f7e34b25f090053260a80de915fea45f22cbd9
wc <chong.wu.xiangfan@gmail.com>
2023-11-13 16:33:23 +0800

    fix

--- api/src/features/frontcodegen/frontcodegen.service.ts
+++ api/src/features/frontcodegen/frontcodegen.service.ts
@@ -36,26 +36,37 @@ export class FrontcodegenService {
     }
   }
 
+  getLabel(columnConfig: any) {
+    if (!columnConfig.comment) {
+      return `(${columnConfig.name})`
+    }
+    return columnConfig.comment
+  }
+
   getTableColumnCode(columnConfig: any) {
     if (columnConfig.dataType.dataType === 'datetime') {
       return `
-        <el-table-column  label="${columnConfig.comment}" >
+        <el-table-column  label="${this.getLabel(columnConfig)}" >
           <template #default="scope">
-            {{ moment(scope.row.${columnConfig.name}).format("YYYY-MM-DD HH:mm") }}
+            {{ moment(scope.row.${
+              columnConfig.name
+            }).format("YYYY-MM-DD HH:mm") }}
           </template>
         </el-table-column>`
     } else if (columnConfig.refTable) {
       return `
-        <el-table-column  label="${columnConfig.comment}"   >
+        <el-table-column  label="${this.getLabel(columnConfig)}"   >
           <template #default="scope">
-            <div v-for="item in ${columnConfig.refTable.className}List ">
-              <el-tag  v-if="scope.row.${columnConfig.name} === item.id" type="" >{{item.name}}</el-tag>
+            <div v-for="item in ${columnConfig.refTable.instanceName}List ">
+              <el-tag  v-if="scope.row.${
+                columnConfig.name
+              } === item.id" type="" >{{item.name}}</el-tag>
             </div>
           </template>
         </el-table-column>`
     } else if (columnConfig.dataType.dataType === 'boolean') {
       return `
-        <el-table-column  label="${columnConfig.comment}"   >
+        <el-table-column  label="${this.getLabel(columnConfig)}"   >
           <template #default="scope">
             <el-tag v-if="scope.row.${columnConfig.name} === true">是</el-tag>
             <el-tag v-else>否</el-tag>
@@ -63,7 +74,9 @@ export class FrontcodegenService {
         </el-table-column>`
     } else {
       return `
-        <el-table-column prop="${columnConfig.name}" label="${columnConfig.comment}"/>`
+        <el-table-column prop="${columnConfig.name}" label="${this.getLabel(
+        columnConfig,
+      )}"/>`
     }
   }
 
@@ -93,7 +106,7 @@ export class FrontcodegenService {
 
     if (formItemCode) {
       return `
-        <el-form-item label="${columnConfig.comment}">
+        <el-form-item label="${this.getLabel(columnConfig)}">
           ${formItemCode}
         </el-form-item>\n`
     }
@@ -115,7 +128,7 @@ export class FrontcodegenService {
     } else if (columnConfig.refTable) {
       return `<el-select v-model="${objectName}.${columnConfig.name}" class="m-2" placeholder="Select" size="large" ${disabledCode}>
                 <el-option
-                  v-for="item in ${columnConfig.refTable.className}List"
+                  v-for="item in ${columnConfig.refTable.instanceName}List"
                   :key="item.id"
                   :label="item.name"
                   :value="item.id"/>
@@ -148,21 +161,50 @@ export class FrontcodegenService {
     return ``
   }
 
+  getViewDialogItem(columnConfig: any) {
+    let textCode = ``
+    if (columnConfig.dataType.dataType === 'datetime') {
+      textCode = `<el-text>{{moment(dialogData.${columnConfig.name}).format("YYYY-MM-DD HH:mm")}}</el-text>`
+    } else if (columnConfig.dataType.dataType === 'boolean') {
+      textCode = `
+        <el-text v-if="dialogData.${columnConfig.name}">是</el-text>
+        <el-text v-else>否</el-text>
+      `
+    } else if (columnConfig.refTable) {
+      textCode = `
+        <el-text v-for="item in ${columnConfig.refTable.instanceName}List" v-if="item.id===dialogData.${columnConfig.name}">{{item.name}}</el-text>
+      `
+    } else {
+      textCode = `<el-text>{{dialogData.${columnConfig.name}}}</el-text>`
+    }
+
+    return `
+      <el-row>
+        <el-col :span="4" style="text-align:right;"><el-text tag="b">${this.getLabel(
+          columnConfig,
+        )}：</el-text></el-col>
+        <el-col :span="20">${textCode}</el-col>
+      </el-row>
+    `
+  }
+
   getRefCode(columnConfig: any) {
     return `
-      const ${columnConfig.refTable.className}List = ref<any>([]);
-      ${columnConfig.refTable.className}List.value = (
-        await devToolApiClient.getAll${columnConfig.refTable.className}({ skipPaging: true })
+      const ${columnConfig.refTable.instanceName}List = ref<any>([]);
+      ${columnConfig.refTable.instanceName}List.value = (
+        await ${columnConfig.refTable.instanceName}Api.getAll${columnConfig.refTable.className}({ skipPaging: true })
       ).rows;\n`
   }
 
   generate(tableConfig: any) {
     let createDialogFieldsCode = `` //新增弹窗内表单项代码
     let updateDialogFieldsCode = `` //修改弹窗内表单项代码
+    let viewDialogItemsCode = `` //查看弹窗内容代码
     let interfacesCode = `` //获取下拉列表项接口代码
     let paramsCode = `` //存取输入框内容变量名代码
     let filtersCode = `` //筛选条件项代码
     let columnsCode = `` //表格列代码
+    let apiFilesCode = `` //引用链接表API文件路径
 
     for (const columnConfig of tableConfig.table.filterItems) {
       if (columnConfig.findable) {
@@ -170,11 +212,19 @@ export class FrontcodegenService {
         paramsCode += `${columnConfig.name}: undefined,\n`
       }
 
+      if (!columnConfig.refTable) {
+        continue
+      }
       if (
-        columnConfig.refTable &&
-        interfacesCode.indexOf(columnConfig.refTable.className + 'List') === -1
+        interfacesCode.indexOf(columnConfig.refTable.instanceName + 'List') ===
+        -1
       ) {
         interfacesCode += this.getRefCode(columnConfig)
+      }
+      if (
+        apiFilesCode.indexOf(columnConfig.refTable.instanceName + 'APi') === -1
+      ) {
+        apiFilesCode += `import * as ${columnConfig.refTable.instanceName}Api from '@/plugins/${columnConfig.refTable.instanceName}.service.ts'\n`
       }
     }
 
@@ -197,6 +247,12 @@ export class FrontcodegenService {
       updateDialogFieldsCode += this.getFieldCode(columnConfig, 'updateDialog')
     }
 
+    for (const columnConfig of tableConfig.table.viewDialogItems) {
+      if (columnConfig.viewable) {
+        viewDialogItemsCode += this.getViewDialogItem(columnConfig)
+      }
+    }
+
     const code = this.renderTemplate(
       columnsCode,
       interfacesCode,
@@ -205,7 +261,9 @@ export class FrontcodegenService {
       tableConfig.className,
       createDialogFieldsCode,
       updateDialogFieldsCode,
+      viewDialogItemsCode,
       tableConfig.instanceName,
+      apiFilesCode,
     )
 
     return this.codeFormat(code)
@@ -219,7 +277,9 @@ export class FrontcodegenService {
     className: string,
     createDialogFieldsCode: string,
     updateDialogFieldsCode: string,
+    viewDialogItemsCode: string,
     instanceName: string,
+    apiFilesCode: string,
   ) {
     return `
       <template>
@@ -234,9 +294,11 @@ export class FrontcodegenService {
                   <el-button :icon="Search" />
                 </template>
               </el-input>
-              <el-button :icon="Plus" class="btn-add" @click="openForm('add')">
-                新建
-              </el-button>
+              ${
+                createDialogFieldsCode
+                  ? `<el-button :icon="Plus" class="btn-add" @click="openForm('add')">新建</el-button>`
+                  : ''
+              }
             </div>
             <el-pagination :current-page="params.page" :page-size="params.pageSize" layout="total, prev, pager, next"
               :total="table.data.count" @current-change="changeCurrentPage"></el-pagination>
@@ -246,9 +308,10 @@ export class FrontcodegenService {
           :data="table.data.rows"
           class="table-body"
           @sort-change="onColumnSort"
-          :row-class-name="tableRowClassName">
+          :row-class-name="tableRowClassName"
+          @row-click="handle">
             ${columnsCode}
-            <el-table-column label="" width="150">
+            <el-table-column label="操作" width="150">
               <template #default="scope">
                 <div
                   style="
@@ -284,11 +347,14 @@ export class FrontcodegenService {
             <div v-else-if="dialog.type === 'edit'">
               ${updateDialogFieldsCode}
             </div>
+            <div v-else-if="dialog.type === 'view'">
+              ${viewDialogItemsCode}
+            </div>
           </el-form>
           <template #header>
             <span class="dialog-footer">
               <el-button @click="dialog.visible = false">关闭</el-button>
-              <el-button type="primary" @click="submit">保存</el-button>
+              <el-button type="primary" @click="submit" v-if="dialog.type!='view'">保存</el-button>
             </span>
           </template>
         </el-dialog>
@@ -296,7 +362,8 @@ export class FrontcodegenService {
 
       <script lang="ts" setup>
         import { ref, watch, computed, shallowRef, reactive, devtools } from "vue";
-        import { devToolApiClient } from "@/plugins";
+        import * as ${instanceName}Api from '@/plugins/${instanceName}.service.ts'
+        ${apiFilesCode}
         import { Search, Plus } from "@element-plus/icons-vue";
         import _ from "lodash";
         import moment from "moment";
@@ -347,7 +414,7 @@ export class FrontcodegenService {
         async function refreshTable() {
           try {
             table.value.loading = true;
-            table.value.data = await devToolApiClient.getAll${className} (
+            table.value.data = await ${instanceName}Api.getAll${className} (
               params.value,
             );
             table.value.loading = false;
@@ -378,12 +445,12 @@ export class FrontcodegenService {
           try {
             dialog.button.loading = true;
             if (dialog.type === "edit") {
-              const result = await devToolApiClient.patch${className}(
+              const result = await ${instanceName}Api.patch${className}(
                 dialogData.value.id,
                 dialogData.value,
                 );
             } else if (dialog.type === "add") {
-              const result = await devToolApiClient.add${className}(dialogData.value);
+              const result = await ${instanceName}Api.add${className}(dialogData.value);
             }
         
             dialog.button.loading = false;
@@ -398,26 +465,38 @@ export class FrontcodegenService {
         async function openForm(openType: String, row?: any) {
           dialog.type = openType;
           if (dialog.type === "edit") {
-            const ${instanceName} = await devToolApiClient.get${className}ById(row.id);
+            const ${instanceName} = await ${instanceName}Api.get${className}ById(row.id);
             dialogData.value = _.cloneDeep(${instanceName});
           } else if (dialog.type === "add") {
             dialogData.value = ref<any>({});
+            ${createDialogFieldsCode ? '' : 'return'}
+          }
+          else if(dialog.type === "view"){
+            ${viewDialogItemsCode ? '' : 'return'}
+            const ${instanceName} = await ${instanceName}Api.get${className}ById(row.id);
+            dialogData.value = _.cloneDeep(${instanceName});
           }
           dialog.visible = true;
         }
 
-        async function deleteData(row: any) {
-          try {
-            await ElMessageBox.confirm("您确定要删除此条数据吗？", "提示", {
-              confirmButtonText: "确认",
-              cancelButtonText: "取消",
-            });
-            const result = await devToolApiClient.delete${className}(row.id);
-            lazyRefreshTable();
-          } catch (e) {
-            ElMessage({ message: '数据删除失败', type: 'warning', })
+        function handle(row: any,column: any){
+          if (column.label != "操作") {
+            openForm('view', row)
           }
-        }
+          }
+
+          async function deleteData(row: any) {
+            try {
+              await ElMessageBox.confirm("您确定要删除此条数据吗？", "提示", {
+                confirmButtonText: "确认",
+                cancelButtonText: "取消",
+              });
+              const result = await ${instanceName}Api.delete${className}(row.id);
+              lazyRefreshTable();
+            } catch (e) {
+              ElMessage({ message: '数据删除失败', type: 'warning', })
+            }
+          }
         
         await lazyRefreshTable();
       </script>
@@ -454,7 +533,10 @@ export class FrontcodegenService {
       
       .btn-add
         margin-left 20px
-  
+      
+      :global(.el-form-item__label)
+        height 40px
+        line-height 40px  
       </style>
     `
   }
