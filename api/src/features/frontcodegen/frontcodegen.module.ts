2398095cc369851f6752f9a90ba3d82f774e68af
w.c <52445078@qq.com>
2024-01-06 23:51:08 +0800

    fix: typo

--- api/src/features/frontcodegen/frontcodegen.module.ts
+++ api/src/features/frontcodegen/frontcodegen.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common'
 import { FrontcodegenController } from './frontcodegen.controller'
 import { FrontcodegenService } from './frontcodegen.service'
-import { BaseModule } from '../base/base.module.js'
+import { BaseModule } from '../base/base.module'
 
 @Module({
   imports: [BaseModule],
