b662139d1fd8b1c6f1b93fca03b74ee8c60003bc
w.c <52445078@qq.com>
2024-01-07 21:10:58 +0800

    refactor: dbsync

--- ui/src/components/SyncPreview.vue
+++ ui/src/components/SyncPreview.vue
@@ -1,7 +1,7 @@
 <template>
   <el-badge :value="toBeSyncCount">
-    <el-button :loading="button.openDialogLoading" @click="open" type="primary"
-      >同步
+    <el-button :loading="button.openDialogLoading" @click="open" type="primary">
+      同步
     </el-button>
   </el-badge>
   <el-dialog title="字段diff" v-model="dialogVisible" width="60%">
@@ -9,21 +9,28 @@
       <tr>
         <th>类型</th>
         <th>语句</th>
-        <th>警告</th>
+        <!-- <th>警告</th> -->
         <th></th>
       </tr>
       <tr v-for="columnDiff in columnDiffs">
-        <td>{{ columnDiff.code }}</td>
-        <td>{{ columnDiff.sql }}</td>
-        <td>{{ columnDiff.error.join(",") }}</td>
         <td>
+          {{ columnDiff.reason }}
+          {{
+            columnDiff.columnModifiedItem
+              ? `(${columnDiff.columnModifiedItem})`
+              : ""
+          }}
+        </td>
+        <td>{{ columnDiff.sql }}</td>
+        <!-- <td>{{ columnDiff.mysqlDefinition }}</td> -->
+        <td>
+          <!-- :disabled="!!columnDiff.error.length" -->
           <el-button
-            :disabled="!!columnDiff.error.length"
             type="danger"
             @click="execSyncSql(columnDiff.sql)"
-            :loading="button.execLoading"
-            >执行</el-button
-          >
+            :loading="button.execLoading">
+            执行
+          </el-button>
         </td>
       </tr>
     </table>
@@ -80,38 +87,6 @@ export default defineComponent({
     async refreshSyncResult() {
       const results = await devToolApiClient.getDBSyncColumnDiffs(this.tableId);
       this.toBeSyncCount = results.length;
-      for (const result of results) {
-        result.error = result.error.map((o: ERROR_REASON) => {
-          switch (o) {
-            case ERROR_REASON.ERROR_CONSTRAINT_NEW:
-              return "数据依赖错误";
-          }
-        });
-        result.code = result.code.map((o: SYNC_REASON) => {
-          switch (o) {
-            case SYNC_REASON.SYNC_CONSTRAINT_NEW:
-              return "新约束";
-            case SYNC_REASON.SYNC_CONSTRAINT_DROP:
-              return "error";
-            case SYNC_REASON.SYNC_CONSTRAINT_MODIFY:
-              return "改约束";
-            case SYNC_REASON.SYNC_ALLOWNULL_MODIFY:
-              return "改null";
-            case SYNC_REASON.SYNC_DEFAULTVALUE_MODIFY:
-              return "改默认值";
-            case SYNC_REASON.SYNC_COMMENT_MODIFY:
-              return "改注释";
-            case SYNC_REASON.SYNC_DTATTYPE_MODIFY:
-              return "改数据类型";
-            case SYNC_REASON.SYNC_FEILD_NEW:
-              return "加列";
-            case SYNC_REASON.SYNC_FEILD_DELETE:
-              return "删列";
-            default:
-              return o;
-          }
-        });
-      }
       this.columnDiffs = results;
     },
     async execSyncSql(sql: string) {
